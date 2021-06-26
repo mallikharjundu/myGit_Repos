787ad5fb3ba35d6b81fb12111eaf61222280eaa5
malli <mallif14@gmail.com>
2021-06-26 18:05:27 +0530

    addfunction in sprint ONE

--- myregpage.java
+++ myregpage.java
@@ -1,2 +1,9 @@
 this is my regpage
+<<<<<<< Updated upstream
 this is my manager work hasa completed
+=======
+this is java code line 01 day 1
+this is java code line 02 day 2
+this is java code line 03 day 3
+this id java code line 04 day 4  80% of work has completed
+>>>>>>> Stashed changes
